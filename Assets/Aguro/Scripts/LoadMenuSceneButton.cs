4c931b23ea0cf9cadd9dd981a9c1fbcdc4cdaa18
aguroshou <aguroshou0413@gmail.com>
2020-10-10 21:06:47 +0900

    AR Foundationのサンプルを入れ、メニューからサンプルシーンを開ける状態です。

---
+++ Assets/Aguro/Scripts/LoadMenuSceneButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMenuSceneButton : MonoBehaviour
+{
+    static public LoadMenuSceneButton instance;
+    void Awake ()
+    {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad (gameObject);
+        }
+        else {
+            Destroy (gameObject);
+        }
+    }
+}

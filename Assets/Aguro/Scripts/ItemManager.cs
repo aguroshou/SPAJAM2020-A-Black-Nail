4426080c889ef568a03ab7da2d74e3b2750efb4f
aguroshou <aguroshou0413@gmail.com>
2020-10-17 22:16:00 +0900

    アイテムを取得する処理を追加しました。

---
+++ Assets/Aguro/Scripts/ItemManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    [SerializeField] private GameObject itemPrefab;
+    public float timeOut = 5.0f;
+    private float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= timeOut)
+        {
+            // Do anything
+
+            timeElapsed = 0.0f;
+            Vector3 prefabPosition = transform.position;
+            prefabPosition.x += Random.Range(-0.13f, 0.13f);
+            prefabPosition.z += Random.Range(-0.13f, 0.13f);
+            Instantiate(itemPrefab, prefabPosition, Quaternion.identity);
+        }
+    }
+}
